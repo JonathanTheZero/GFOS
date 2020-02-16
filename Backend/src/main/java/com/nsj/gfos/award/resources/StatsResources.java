628f36e1a599a549f8d7ec0fd3757cd816757b5d
Sophie <56729612+artemis-zip@users.noreply.github.com>
2020-02-16 21:57:54 +0100

    Add files via upload

---
+++ Backend/src/main/java/com/nsj/gfos/award/resources/StatsResources.java
@@ -0,0 +1,28 @@
+package com.nsj.gfos.award.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.sql.ResultSet;
+
+import com.nsj.gfos.award.handlers.JsonHandler;
+import com.nsj.gfos.award.handlers.QueryHandler;
+
+@Path("stats")
+public class StatsResources {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getMitarbeiterCount() {
+		String stmt = "SELECT COUNT(*) AS Anzahl_Mitarbeiter FROM gfos.mitarbeiter";
+		try {
+			ResultSet rs = QueryHandler.query(stmt);
+			if(!rs.next())
+				return JsonHandler.fehler("Es ist ein Fehler in der Datenbankl aufgetreten");
+		} catch (SQLException e) {JsonHandler.fehler("Es ist ein Fehler in der Datenbankl aufgetreten");}
+	}
+		return JsonHandler.erfolg("Es wurden " + rs.getString("Anzahl_Mitarbeiter") + " Mitarbeiter gefunden.");
+	}
+	
+}
