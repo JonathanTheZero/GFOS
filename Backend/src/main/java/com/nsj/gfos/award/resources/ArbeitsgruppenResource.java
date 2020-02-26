73e988447504bfb65e0f72c64645c654c617980f
Sophief <56729612+artemis-zip@users.noreply.github.com>
2020-02-26 22:01:28 +0100

    ArbeitsgruppenResource vollständig

--- Backend/src/main/java/com/nsj/gfos/award/resources/ArbeitsgruppenResource.java
+++ Backend/src/main/java/com/nsj/gfos/award/resources/ArbeitsgruppenResource.java
@@ -19,60 +19,54 @@ import java.util.ArrayList;
 
 @Path("arbeitsgruppe")
 public class ArbeitsgruppenResource {
-	
-	
+
 	@GET
-    @Path("getArbeitsgruppe{attributes}")
-    @Produces(MediaType.APPLICATION_JSON)
+	@Path("getArbeitsgruppe{attributes}")
+	@Produces(MediaType.APPLICATION_JSON)
 	public static String getArbeitsgruppeFromMitarbeiter(@PathParam("attributes") String query) {
 		query = query.substring(1);
-    	String[] attributes = query.split("&");
-    	if(attributes.length != 2)
-    		return JsonHandler.fehler("Falsche Anzahl an Parametern.");
-    	for(String attribute: attributes) {
-    		if(attribute.split("=").length != 2)
-    			return JsonHandler.fehler("Parameter sind falsch formatiert.");
-    	}
-    	String auth = attributes[0].split("=")[1];
-    	String pn = attributes[1].split("=")[1];
-    	if(pn.length() != 12)
-    		return JsonHandler.fehler("Ungültige Personalnummer.");
-    	//TODO checkRights
-    	if(!SessionHandler.checkSessionID(auth))
-    		return JsonHandler.fehler("SessionID ist ungültig.");
-    	String sqlStmt = "SELECT gfos.arbeitsgruppe.ArbeitsgruppenID, \r\n" + 
-    			"       gfos.arbeitsgruppe.Bezeichnung, \r\n" + 
-    			"       gfos.arbeitsgruppe.Leiter, \r\n" + 
-    			"       gfos.arbeitsgruppenteilnahme.Personalnummer \r\n" + 
-    			"FROM   gfos.arbeitsgruppenteilnahme \r\n" + 
-    			"       JOIN gfos.arbeitsgruppe \r\n" + 
-    			"         ON gfos.arbeitsgruppenteilnahme.ArbeitsgruppenID = \r\n" + 
-    			"            gfos.arbeitsgruppe.ArbeitsgruppenID \r\n" + 
-    			"WHERE \r\n" + 
-    			"(SELECT gfos.arbeitsgruppenteilnahme.ArbeitsgruppenID FROM gfos.arbeitsgruppenteilnahme WHERE gfos.arbeitsgruppenteilnahme.Personalnummer = \"" + pn + "\")\r\n" + 
-    			"= gfos.arbeitsgruppe.ArbeitsgruppenID;";
-    	try {
+		String[] attributes = query.split("&");
+		if (attributes.length != 2)
+			return JsonHandler.fehler("Falsche Anzahl an Parametern.");
+		for (String attribute : attributes) {
+			if (attribute.split("=").length != 2)
+				return JsonHandler.fehler("Parameter sind falsch formatiert.");
+		}
+		String auth = attributes[0].split("=")[1];
+		String pn = attributes[1].split("=")[1];
+		if (pn.length() != 12)
+			return JsonHandler.fehler("Ungültige Personalnummer.");
+		// TODO checkRights
+		if (!SessionHandler.checkSessionID(auth))
+			return JsonHandler.fehler("SessionID ist ungültig.");
+		String sqlStmt = "SELECT gfos.arbeitsgruppe.ArbeitsgruppenID, \r\n"
+				+ "       gfos.arbeitsgruppe.Bezeichnung, \r\n" + "       gfos.arbeitsgruppe.Leiter, \r\n"
+				+ "       gfos.arbeitsgruppenteilnahme.Personalnummer \r\n" + "FROM   gfos.arbeitsgruppenteilnahme \r\n"
+				+ "       JOIN gfos.arbeitsgruppe \r\n"
+				+ "         ON gfos.arbeitsgruppenteilnahme.ArbeitsgruppenID = \r\n"
+				+ "            gfos.arbeitsgruppe.ArbeitsgruppenID \r\n" + "WHERE \r\n"
+				+ "(SELECT gfos.arbeitsgruppenteilnahme.ArbeitsgruppenID FROM gfos.arbeitsgruppenteilnahme WHERE gfos.arbeitsgruppenteilnahme.Personalnummer = \""
+				+ pn + "\")\r\n" + "= gfos.arbeitsgruppe.ArbeitsgruppenID;";
+		try {
 			ResultSet rs = QueryHandler.query(sqlStmt);
-			if(!rs.next())
+			if (!rs.next())
 				return JsonHandler.fehler("Leere Rückgabe der Datenbank.");
 			Arbeitsgruppe a = createArbeitsgruppeFromQuery(rs);
 			return JsonHandler.createJsonFromArbeitsgruppe(a);
 		} catch (SQLException e) {
 			return JsonHandler.fehler(e.toString());
 		}
-		return "";
+	}
 
-		}
-	
 	private static Arbeitsgruppe createArbeitsgruppeFromQuery(ResultSet rs) throws SQLException {
 		Arbeitsgruppe a = new Arbeitsgruppe();
 		a.setBezeichnung(rs.getString("Bezeichnung"));
 		a.setLeiter(rs.getString("Leiter"));
 		do {
 			a.addMitglied(rs.getString("Personalnummer"));
-			
-		}while(rs.next());
+
+		} while (rs.next());
 		return a;
 	}
-	
+
 }
