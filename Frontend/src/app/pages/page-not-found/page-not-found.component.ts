5c479f5b1d2fc968df75ea636bc16717b36dbc98
JonathanTheZero <jonathan.frohne@web.de>
2020-02-26 19:24:00 +0100

    removed jQuery completely

--- Frontend/src/app/pages/page-not-found/page-not-found.component.ts
+++ Frontend/src/app/pages/page-not-found/page-not-found.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import * as $ from "jquery";
-
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
