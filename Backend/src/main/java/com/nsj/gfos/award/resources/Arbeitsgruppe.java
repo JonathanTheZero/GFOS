5141896bfc195e55ae707e2a919fe2a9705f52c7
Sophief <56729612+artemis-zip@users.noreply.github.com>
2020-02-28 14:47:20 +0100

    neue Methode: alterLeiter

---
+++ Backend/src/main/java/com/nsj/gfos/award/resources/Arbeitsgruppe.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.nsj.gfos.award.dataWrappers;
+
+import java.util.ArrayList;
+
+public class Arbeitsgruppe {
+    
+    private ArrayList<String> mitglieder;
+    private String leiter;
+    private String bezeichnung;
+    
+    public Arbeitsgruppe() {
+    	mitglieder = new ArrayList<String>();
+    	leiter = "";
+    	bezeichnung = "";
+    }
+
+    public ArrayList<String> getMitglieder() {
+        return mitglieder;
+    }
+
+    public void setMitglieder(ArrayList<String> mitglieder) {
+        this.mitglieder = mitglieder;
+    }
+
+    public String getLeiter() {
+        return leiter;
+    }
+
+    public void setLeiter(String leiter) {
+        this.leiter = leiter;
+    }
+
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    public void setBezeichnung(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
+    }
+    
+    public void addMitglied(String mitglied) {
+        if(mitglied != null)
+            mitglieder.add(mitglied);
+    }
+    
+    
+}
