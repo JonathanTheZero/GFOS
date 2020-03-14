0a46dc7d4e4880961c885676eb6b53622d94cfbc
JonathanTheZero <jonathan.frohne@web.de>
2020-03-14 15:09:05 +0100

    Finally all UI bugs fixed

--- Frontend/src/app/ui/layout/header/header.component.ts
+++ Frontend/src/app/ui/layout/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
   public readonly headerLinks = [
