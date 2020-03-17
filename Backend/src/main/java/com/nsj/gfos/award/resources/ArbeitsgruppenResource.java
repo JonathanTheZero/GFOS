6db7b003cd60f17345fbe9fa2426239b836ba103
Sophief <56729612+artemis-zip@users.noreply.github.com>
2020-03-17 22:43:27 +0100

    get weg

--- Backend/src/main/java/com/nsj/gfos/award/resources/ArbeitsgruppenResource.java
+++ Backend/src/main/java/com/nsj/gfos/award/resources/ArbeitsgruppenResource.java
@@ -84,47 +84,6 @@ public class ArbeitsgruppenResource {
 			return JsonHandler.fehler(e.toString());
 		}
 	}
-	
-	/**
-	 * Die Methode <i>getArbeitsgruppe</i> gibt die Arbeitsgruppe mit Hilfe ihrer ID zurück.
-	 * 
-	 * @param auth - SessionID des ausführenden Mitarbeiters
-	 * @param id   - ArbeitsgruppenID von der Abreitsgruppe, die man haben möchte
-	 * @return String - Arbeitsgruppe als Json
-	 */
-	@GET
-	@Path("get{attributes}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public static String getArbeitsgruppe(@PathParam("attributes") String query) {
-		query = query.substring(1);
-		String[] attributes = query.split("&");
-		if (attributes.length != 2)
-			return JsonHandler.fehler("Falsche Anzahl an Parametern.");
-		for (String attribute : attributes) {
-			if (attribute.split("=").length != 2)
-				return JsonHandler.fehler("Parameter sind falsch formatiert.");
-		}
-		String auth = attributes[0].split("=")[1];
-		String id = attributes[1].split("=")[1];
-		if (id.length() != 12)
-			return JsonHandler.fehler("ID ist ungültig");
-		if (!SessionHandler.checkSessionID(auth))
-			return JsonHandler.fehler("SessionID ist ungültig.");
-		if (!Utils.checkIfArbeitsgruppeExistsFromID(id))
-			return JsonHandler.fehler("Arbeitsgruppe existiert nicht.");
-		if(!(Utils.isInArbeitsgruppe(id, Utils.getPnFromSessionID(auth)) || RightHandler.checkPermission(auth, "getArbeitsgruppe")))
-			return JsonHandler.fehler("Der Mitarbeiter hat keine Berechtigung.");
-		String sqlStmt = "SELECT gfos.arbeitsgruppe.ArbeitsgruppenID, gfos.arbeitsgruppe.Bezeichnung, gfos.arbeitsgruppe.Leiter FROM gfos.arbeitsgruppe WHERE gfos.arbeitsgruppe.ArbeitsgruppenID = \"" + id + "\";";
-		try {
-			ResultSet rs = QueryHandler.query(sqlStmt);
-			if (!rs.next())
-				return JsonHandler.fehler("Leere Rückgabe der Datenbank.");
-			Arbeitsgruppe a = Utils.createArbeitsgruppeFromQuery(rs);
-			return JsonHandler.createJsonFromArbeitsgruppe(a);
-		}catch (SQLException e) {
-			return JsonHandler.fehler(e.toString());
-		}
-	}
 
 	/**
 	 * Die Methode <i>alterLeiter</i> verändert den Leiter der Arbeitsgruppe.
