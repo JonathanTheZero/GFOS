51f3bc223bfb6fea9186178c5cd5e7f101a1d71b
= <nils.sommerfeld@gmx.net>
2020-03-21 23:31:52 +0100

    QueryHandler

--- Backend/src/main/java/com/nsj/gfos/award/handlers/QueryHandler.java
+++ Backend/src/main/java/com/nsj/gfos/award/handlers/QueryHandler.java
@@ -6,8 +6,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * Die Klasse <i>QueryHandler</i> stellt Methoden bereit, um SQL-Statements auf der Datenbank auszuführen 
+ * author Schnuels
+ */
 public class QueryHandler {
     
+    /**
+     * Die Methode <i>query</i> führt ein SQL-Statement durch und gibt die Ergebnisse des SELECT Befehls zurück.
+     * @param stmt - SQL-Statement der Query
+     * @return ResultSet - das von der Query zurückgegebene Objekt vom Typ ResultSet
+     * @throws SQLException
+     */
     public static ResultSet query(String stmt) throws SQLException{
         
         Connection myConn = null;
@@ -24,6 +34,13 @@ public class QueryHandler {
         return myRs;
     }
     
+    /**
+     * Die Methode <i>update</i> führt ein SQL-Statement mit einem UPDATE, DELETE oder INSERT Befehl durch.
+     * @param stmt - SQL-Statement des Updates
+     * @return int - -1, wenn das Statement nicht ausführbar ist, und 0, wenn bei der Durchführung des Updates
+     *               ein Fehler auftritt und die Aktion nicht durchgeführt werden kann
+     * @throws SQLException
+     */
     public static int update(String stmt) throws SQLException{
         
         Connection myConn = null;
