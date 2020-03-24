b4913f6695ee462a8b0362022652abd85a18f3f2
Sophief <56729612+artemis-zip@users.noreply.github.com>
2020-03-24 20:20:06 +0100

    getGemeinsameGruppe

--- Backend/src/main/java/com/nsj/gfos/award/resources/ArbeitsgruppenResource.java
+++ Backend/src/main/java/com/nsj/gfos/award/resources/ArbeitsgruppenResource.java
@@ -151,6 +151,48 @@ public class ArbeitsgruppenResource {
 			return JsonHandler.fehler(e.toString());
 		}
 	}
+	
+	/**
+	 * Die Methode <i>getGemeinsameGruppen</i> gibt die gemeinsamen Arbeitsgruppen des ausführenden und des in den Parametern eingetragenen Mitarbeiters zurück.
+	 * 
+	 * @param auth - SessionID des anfordernden Mitarbeiters
+	 * @param id2   - Personalnummer des anderen Mitarbeiters
+	 * @return String - gemeinsame Arbeitsgruppen beider Mitarbeiter
+	 */
+	@GET
+	@Path("getGemeinsameGruppe{attributes}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public static String getGemeinsameGruppen(@PathParam("auth") String entAuth, @PathParam("id2") String entID) {
+		String[] auth = entAuth.split("=");
+		String id1 = Utils.getPersonalnummerFromSessionID(auth[1]);
+		String[] id2 = entID.split("=");
+		if (auth.length != 2 || id2.length != 2)
+			return JsonHandler.fehler("Falsche Parameter Syntax.");
+		if (auth[1].length() != 12)
+			return JsonHandler.fehler("Parameter sind falsch formatiert.");
+		if(Utils.checkIfMitarbeiterExists(id1) || Utils.checkIfMitarbeiterExists(id2[1]))
+			return JsonHandler.fehler("Mindestens ein Mitarbeiter existiert nicht.");
+		String sqlStmt = "SELECT DISTINCT a.ArbeitsgruppenID FROM(SELECT * FROM gfos.arbeitsgruppenteilnahme WHERE gfos.arbeitsgruppenteilnahme.Mitarbeiter = \"" + id1 + "\") AS a INNER JOIN (SELECT * FROM gfos.arbeitsgruppenteilnahme WHERE gfos.arbeitsgruppenteilnahme.Mitarbeiter = \"" + id2[1] + "\") AS b ON a.ArbeitsgruppenID = b.ArbeitsgruppenID;";
+		ObjectMapper om = new ObjectMapper();
+		ArrayList<Arbeitsgruppe> arbeitsgruppen = new ArrayList<Arbeitsgruppe>();
+		try {
+			ResultSet rs = QueryHandler.query(sqlStmt);
+			while(rs.next()) {
+				Arbeitsgruppe a = new Arbeitsgruppe();
+				a.setBezeichnung(Utils.getBezeichnung(rs.getString("ArbeitsgruppenID")));
+				a.setLeiter(Utils.getLeiter(rs.getString("ArbeitsgruppenID")));
+				a.setArbeitsgruppenID(rs.getString("ArbeitsgruppenID"));
+				arbeitsgruppen.add(a);
+			}
+		} catch (SQLException e) {
+			return JsonHandler.fehler(e.toString());
+		}
+		try {
+			return om.writeValueAsString(arbeitsgruppen);
+		} catch (Exception e) {
+			return JsonHandler.fehler(e.toString());
+		}
+	}
 
 	/**
 	 * Die Methode <i>alterLeiter</i> verÃ¤ndert den Leiter der Arbeitsgruppe.
