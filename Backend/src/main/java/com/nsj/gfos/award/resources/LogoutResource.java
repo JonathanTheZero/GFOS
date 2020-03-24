7455e7e0ac3ec143f6b04d5152299223f26f0e3a
Schnuels <nils.sommerfeld@gmx.net>
2020-03-24 22:12:43 +0100

    Logout umgeschrieben

--- Backend/src/main/java/com/nsj/gfos/award/resources/LogoutResource.java
+++ Backend/src/main/java/com/nsj/gfos/award/resources/LogoutResource.java
@@ -18,7 +18,7 @@ import com.nsj.gfos.award.handlers.SessionHandler;
  * 
  * @author Schnuels
  */
-@Path("logout{sessionID}")
+@Path("logout:{sessionID}")
 public class LogoutResource {
 
 	/**
@@ -27,14 +27,13 @@ public class LogoutResource {
 	 * <i>SessionHandler</i> weiter.
 	 * 
 	 * @param sessionID - Teil der URL nach 'logout' also zum Beispiel
-	 *                  ':auth=123456789012'
+	 *                  'auth=123456789012'
 	 * @return String - Fehler bei falscher Formatierung des Parameters oder die
 	 *         Rückgabe von <i>SessionHandler.closeSession(...)</i>
 	 */
-	@GET	
+	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public String logout(@PathParam("sessionID") String sessionID) {
-		sessionID = sessionID.substring(1);
 		if (sessionID.split("=").length != 2)
 			return JsonHandler.fehler("Falsche Formatierung des Parameters.");
 		String auth = sessionID.split("=")[1];
@@ -46,19 +45,18 @@ public class LogoutResource {
 	}
 
 	/**
-	 * Die Methode <i>logoutPost</i> wird bei einer POST Anfrage an diese Resource aufgerufen
-	 * und verarbeitet die als Parameter gegebene SessionID und gibt diese an den
-	 * <i>SessionHandler</i> weiter.
+	 * Die Methode <i>logoutPost</i> wird bei einer POST Anfrage an diese Resource
+	 * aufgerufen und verarbeitet die als Parameter gegebene SessionID und gibt
+	 * diese an den <i>SessionHandler</i> weiter.
 	 * 
 	 * @param sessionID - Teil der URL nach 'logout' also zum Beispiel
-	 *                  ':auth=123456789012'
+	 *                  'auth=123456789012'also icg
 	 * @return String - Fehler bei falscher Formatierung des Parameters oder die
 	 *         Rückgabe von <i>SessionHandler.closeSession(...)</i>
 	 */
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	public String logoutPost(@PathParam("sessionID") String sessionID) {
-		sessionID = sessionID.substring(1);
 		if (sessionID.split("=").length != 2)
 			return JsonHandler.fehler("Falsche Formatierung des Parameters.");
 		String auth = sessionID.split("=")[1];
