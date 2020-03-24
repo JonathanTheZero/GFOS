2fc2dc2f03105deab03654eaa1c6ded13dd23d45
JonathanTheZero <jonathan.frohne@web.de>
2020-03-24 21:04:13 +0100

    little check on employee stats component

--- Frontend/src/app/utils/pipes/list-employees.pipe.ts
+++ Frontend/src/app/utils/pipes/list-employees.pipe.ts
@@ -10,12 +10,10 @@ export class ListEmployeesPipe implements PipeTransform {
   constructor(private namePipe: EmployeeNamePipe) {}
 
   transform(value: Mitarbeiter[], length = 2): string {
-
-    if(value.length > 2) return `${value[0].vorname} ${value[0].name}, ${value[1].vorname} ${value[1].name}...`;
-
     let str: string = "";
-    for(var i = 0; i < value.length; i++) {
-        str += `${value[i].vorname} ${value[i].name}, `
+    
+    for(var i = 0; i < value.length && i < length; i++) {
+        str += `${this.namePipe.transform(value[i])}, `
     }
 
     return str.substr(0, str.length - 2);
